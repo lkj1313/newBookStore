60cb62b01aaf420967cf44cea9ed2e42898a8f34
unknown <smm45108@gmail.com>
2024-10-03 18:56:59 +0900

    feat: Complete footer implementation on HomePage

--- frontend/src/shared/ui/Nav.tsx
+++ frontend/src/shared/ui/Nav.tsx
@@ -3,7 +3,7 @@ import RightNav from "./RightNav";
 
 const Nav = () => {
   return (
-    <div className="border-b border-gray-200 sticky z-10 top-0 dark:bg-slate-950">
+    <div className="border-b border-gray-200 bg-gray-100 sticky z-10 top-0 dark:bg-slate-950">
       <div className="lg:max-w-screen-lg mx-auto ">
         <div className=" flex flex-row justify-between pt-4 pb-4 ">
           <LeftNav />
