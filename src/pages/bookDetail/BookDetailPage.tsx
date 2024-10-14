495bdb3ef1a8acfe8d64a10044d1e16035892985
unknown <smm45108@gmail.com>
2024-10-14 18:58:45 +0900

    1

--- src/pages/bookDetail/BookDetailPage.tsx
+++ src/pages/bookDetail/BookDetailPage.tsx
@@ -3,7 +3,7 @@ import { useQuery } from "@tanstack/react-query";
 
 const fetchBookByIsbn = async (isbn: string) => {
   const response = await fetch(
-    `https://new-book-store-4fo2.vercel.app/api/search/book?query=${isbn}`
+    `https://https://new-book-store-7m3b.vercel.app//api/search/book?query=${isbn}`
   );
   if (!response.ok) {
     throw new Error("Failed to fetch book data");
